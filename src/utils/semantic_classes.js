307876eecd3e30215ba5f6f474e89f2dde65bc0e
Armand Zerilli <armand@zerilliworks.net>
2016-02-18 15:30:42 -0500

    Add semantic classes util

---
+++ src/utils/semantic_classes.js
@@ -0,0 +1,31 @@
+import _ from 'lodash'
+
+const numberWords = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen'
+}
+
+export default class SemanticClasses {
+  static numberToClass(number) {
+    return _.get(numberWords, number, '')
+  }
+
+  static classToNumber(nclass) {
+    return _.get(_.invert(numberWords), nclass, '')
+  }
+}
