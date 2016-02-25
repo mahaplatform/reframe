8ebdd71e7784ee5a82a3320d587e5b1078f5709f
Armand Zerilli <armand@zerilliworks.net>
2016-02-25 15:25:58 -0500

    Implement automatic releases

--- release.sh
+++ release.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 mkdir _release
+git clone -b release git@github.com:thinktopography/reframe _release
 cd _release
-#git clone -b release git@github.com:thinktopography/reframe
 cp -f ../package.json ./package.json
 babel -d ./ ../src/
 sass -Cq --scss --compass --sourcemap=none ../src/reframe.scss ./reframe.css
+
 VER=`cat package.json | sed -n 's/.*"version": "\(.*\)".*/\1/p'`
 
-echo $VER
+if (git tag | grep $VER); then
+    echo "Error: release $VER already exists"; exit 1
+fi
 
-#git add .
-#git commit -m "Release $VER" -e
+git add .
+git commit -m "Release $VER" -e
+git push
+cd ../
+rm -rf _release
