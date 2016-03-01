d6365127b278de3ca81cba13c061cfab7b23f68a
Armand Zerilli <armand@zerilliworks.net>
2016-03-01 11:19:38 -0500

    Build SCSS files with examples

--- examples.sh
+++ examples.sh
@@ -4,4 +4,5 @@ export NODE_ENV=development
 
 trap 'kill %1' SIGINT
   ./node_modules/http-server/bin/http-server -p 8080 \
-& watchify ./examples/index.js -v --debug -t [babelify] -o ./examples/examples.js # 'exorcist ./examples/examples.js.map > ./examples/examples.js'
+& watchify ./examples/index.js -v --debug -t [babelify] -o 'exorcist ./examples/examples.js.map > ./examples/examples.js' \
+& sass --scss --compass --watch --sourcemap=auto src/reframe.scss:examples/reframe.css
