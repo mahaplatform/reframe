1558fc4f1aa36ee2fb7c1f103b90a8d20ae4cc97
Armand Zerilli <armand@zeril.li>
2016-03-02 16:00:23 -0500

    Clean up code on FetchContainer

--- src/containers/fetch/index.js
+++ src/containers/fetch/index.js
@@ -6,55 +6,55 @@ import whenKeys from 'when/keys'
 import API from '../../api'
 
 const UNINITIALIZED = 'uninitialized'
-const AWAITING      = 'awaiting'
-const READY         = 'ready'
-const ERROR         = 'error'
+const AWAITING = 'awaiting'
+const READY = 'ready'
+const ERROR = 'error'
 
 export default class FetchContainer extends React.Component {
   static propTypes = {
-    endpoint:        React.PropTypes.string.isRequired,
+    endpoint: React.PropTypes.string.isRequired,
     endpointOptions: React.PropTypes.object.isRequired,
-    blocking:        React.PropTypes.bool,
-    single:          React.PropTypes.bool,
-    mapper:          React.PropTypes.func,
-    transformer:     React.PropTypes.func,
-    element:         React.PropTypes.string,
-    flatten:         React.PropTypes.bool,
-    injectAs:        React.PropTypes.string
+    blocking: React.PropTypes.bool,
+    single: React.PropTypes.bool,
+    mapper: React.PropTypes.func,
+    transformer: React.PropTypes.func,
+    element: React.PropTypes.string,
+    flatten: React.PropTypes.bool,
+    injectAs: React.PropTypes.string
   }
 
   static defaultProps = {
     endpointOptions: {},
-    single:          false,
-    blocking:        false,
-    mapper:          _.identity,
-    transformer:     _.identity,
-    element:         'div',
-    client:          undefined, // Causes API to use default client
-    flatten:         false,
-    injectAs:        'data'
+    single: false,
+    blocking: false,
+    mapper: _.identity,
+    transformer: _.identity,
+    element: 'div',
+    client: undefined, // Causes API to use default client
+    flatten: false,
+    injectAs: 'data'
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      status:       AWAITING,
+      status: AWAITING,
       endpointData: null,
-      propsData:    null,
-      message:      null
+      propsData: null,
+      message: null
     }
 
     this.api = new API(this.props.client)
 
     const propsPromises = _(this.props)
-      .omit(['className', 'endpoint', 'client', 'element', 'endpointOptions', 'children'])
+      .omit([ 'className', 'endpoint', 'client', 'element', 'endpointOptions', 'children' ])
       .mapValues(p => when(p))
       .value()
 
-    const endpointPromise    = this.api.loadJSON(this.props.endpoint, this.props.endpointOptions)
+    const endpointPromise = this.api.loadJSON(this.props.endpoint, this.props.endpointOptions)
     const propsPromiseObject = whenKeys.all(propsPromises)
 
-    const attributesPromise = when.all([endpointPromise, propsPromiseObject])
+    const attributesPromise = when.all([ endpointPromise, propsPromiseObject ])
       .then(([endpointData, propsData]) => this.setState({ status: READY, endpointData, propsData }))
       .catch(e => this.setState({ status: ERROR, message: e }))
       .finally(_.noop)
@@ -63,12 +63,12 @@ export default class FetchContainer extends React.Component {
   render() {
     let finalProps = {}
 
-    if (this.state.endpointData) {
+    if ( this.state.endpointData ) {
 
       _.assign(finalProps, this.state.propsData)
 
-      if (this.props.single) {
-        if (this.props.flatten) {
+      if ( this.props.single ) {
+        if ( this.props.flatten ) {
           _.assign(
             finalProps,
             _.toPairs(this.state.endpointData)
@@ -82,9 +82,9 @@ export default class FetchContainer extends React.Component {
             finalProps,
             {
               [this.props.injectAs]: _.toPairs(this.state.endpointData)
-                                       .map(([key, val]) => this.props.mapper(val, key))
-                                       .fromPairs()
-                                       .value()
+                .map(([key, val]) => this.props.mapper(val, key))
+                .fromPairs()
+                .value()
             }
           )
         }
@@ -97,7 +97,7 @@ export default class FetchContainer extends React.Component {
 
     let mappedChildren = []
 
-    if (!this.props.blocking || this.state.status === READY) {
+    if ( !this.props.blocking || this.state.status === READY ) {
       mappedChildren = React.Children.map(this.props.children, c => React.cloneElement(c, finalProps))
     }
 
