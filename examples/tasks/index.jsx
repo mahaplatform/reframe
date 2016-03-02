abcd7c20ebd6a1446ef4590354cd976ef683f2ed
mochini <greg@thinktopography.com>
2016-03-02 15:08:55 -0500

    merged

---
+++ examples/tasks/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import tasks from 'tasks'
+
+export default class TasksExamples extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>Tasks</h1>
+
+        <div className="ui top attached segment">
+          <Tasks tasks=getTasks() />
+        </div>
+      </div>
+    )
+  }
+
+  getTasks() {
+    return [
+      { label: 'Details', link: '/admin/contact/12345' },
+      { label: 'Edit', link: '/admin/contact/12345/edit' },
+    ]
+  }
+
+}
---
+++ src/tasks/task.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {IndexLink} from 'react-router'
+
+class Task extends React.Component {
+
+  render() {
+    if(this.props.task.handler) {
+      return <a onClick={this.props.task.handler} activeClassName="active" className="item">{this.props.task.label}</a>
+    } else if(this.props.task.route) {
+      return <IndexLink to={this.props.task.route} activeClassName="active" className="item">{this.props.task.label}</IndexLink>
+    } else if(this.props.task.link) {
+      return <IndexLink to={this.props.task.link} activeClassName="active" className="item">{this.props.task.label}</IndexLink>
+    }
+  }
+  
+}
+
+export default Task
