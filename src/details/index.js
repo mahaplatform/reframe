f1bc355e2deeed2e05b080231a6c3058acd9f28c
mochini <greg@thinktopography.com>
2016-03-03 15:41:16 -0500

    adjusting details to use 1 details config object

--- src/details/index.js
+++ src/details/index.js
@@ -5,33 +5,35 @@ import Format from '../format'
 class Detail extends React.Component {
 
   static propTypes = {
-    title: React.PropTypes.string,
-    properties: React.PropTypes.array
+    details: React.PropTypes.shape({
+      title: React.PropTypes.string,
+      properties: React.PropTypes.array
+    })
   }
 
   static defaultProps = {
   }
 
   render() {
-    var total = this.props.properties.length;
+    var total = this.props.details.properties.length;
     var half = Math.ceil(total / 2);
-    var left = this.props.properties.splice(0, half);
-    var right = this.props.properties.splice(0, half);
+    var left = this.props.details.properties.splice(0, half);
+    var right = this.props.details.properties.splice(0, half);
     if(total % 2 == 1) {
       right.push({ label: '&nbsp;', value: null })
     }
     return (
       <div className="details">
-        <div className="details-header">{this.props.title}</div>
-        { [left,right].map((collection) => {
+        <div className="details-header">{this.props.details.title}</div>
+        { [left,right].map((collection,index1) => {
           return(
-            <table className="ui small unstackable definition table">
+            <table className="ui small unstackable definition table" key={`detail_column_${index1}`}>
               <tbody>
-              { collection.map((property, index) => {
+              { collection.map((property, index2) => {
                 return (
-                  <tr>
+                  <tr key={`detail_column_${index1}_row_${index2}`}>
                     <td dangerouslySetInnerHTML={{__html: property.label}}></td>
-                    <td><Format {...this.props} format={property.format} value={property.value} key={`property_${index}`} /></td>
+                    <td><Format {...this.props} format={property.format} value={property.value} /></td>
                   </tr>
                 )
               })}
