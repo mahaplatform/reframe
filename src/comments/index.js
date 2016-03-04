4987a34f0f9e61b0eeaff2315bf21650b72a445f
mochini <greg@thinktopography.com>
2016-03-04 12:24:51 -0500

    use loading container

--- src/comments/index.js
+++ src/comments/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Comment from './comment.js'
-import _ from 'lodash'
+import LoadingContainer, {PresentState, EmptyState} from 'snax/lib/containers/loading'
 
 class Comments extends React.Component {
 
@@ -15,17 +15,20 @@ class Comments extends React.Component {
   }
 
   render() {
-    if(_.isEmpty(this.props.records)) {
-      return <p>{this.props.empty}</p>
-    } else {
-      return (
-        <div className="ui comments">
-          { this.props.records.map((record) => {
-            return <Comment {...record} />
-          })}
-        </div>
-      )
-    }
+    return (
+      <LoadingContainer content={this.props.records} useLoader>
+        <EmptyState>
+          <p>{this.props.empty}</p>
+        </EmptyState>
+        <PresentState>
+          <div className="ui feed">
+            { this.props.records.map((record, index) => {
+              return <Comment {...record} key={`event_${index}`} />
+            })}
+          </div>
+        </PresentState>
+      </LoadingContainer>
+    )
   }
 
 }
