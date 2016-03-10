048cf2252725c84a29d36a6bef379dbb9159a98d
Armand Zerilli <armand@zerilliworks.net>
2016-03-10 13:29:26 -0500

    Update release script to exit with error when code fails to build

--- release.sh
+++ release.sh
@@ -5,19 +5,24 @@ mkdir _release
 git clone -b release git@github.com:thinktopography/reframe _release
 cd _release
 cp -f ../package.json ./package.json
-../node_modules/babel-cli/bin/babel.js -d ./ ../src/
-sass -Cq --scss --compass --sourcemap=none ../src/reframe.scss ./reframe.css
-cp ./reframe.css ./reframe.scss
+if (../node_modules/babel-cli/bin/babel.js -d ./ ../src/); then
+  sass -Cq --scss --compass --sourcemap=none ../src/reframe.scss ./reframe.css
+  cp ./reframe.css ./reframe.scss
 
-VER=`cat package.json | sed -n 's/.*"version": "\(.*\)".*/\1/p'`
+  VER=`cat package.json | sed -n 's/.*"version": "\(.*\)".*/\1/p'`
 
-if (git tag | grep $VER); then
-    echo "Error: release $VER already exists"; exit 1
+  if (git tag | grep $VER); then
+      echo "Error: release $VER already exists"; exit 1
+  fi
+
+  git add .
+  git commit -m "Release $VER" -e
+  git tag "$VER"
+  git push
+else
+  echo " "
+  echo "There were errors compiling your code. Please fix these problems before publishing a release."; exit 1
 fi
 
-git add .
-git commit -m "Release $VER" -e
-git tag "$VER"
-git push
 cd ../
 rm -rf _release
