caccad7b29d26ec33a2fa4814727ad83c4374c82
Armand Zerilli <armand@zerilliworks.net>
2016-04-01 16:32:47 -0400

    Release 0.17.2 Fixed an export statement on the Form module generating non-ES5 code (The process of discovering this was, in a word, bazonkers)

--- form/index.js
+++ form/index.js
@@ -4,18 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _core = require('./core');
-
-for (let _key in _core) {
-  if (_key === "default") continue;
-  Object.defineProperty(exports, _key, {
-    enumerable: true,
-    get: function () {
-      return _core[_key];
-    }
-  });
-}
-
 var _omni = require('./omni');
 
 var _omni2 = _interopRequireDefault(_omni);
