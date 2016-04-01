1fe9a8f1b9368a9445b909bec32316d84d44a343
Armand Zerilli <armand@zerilliworks.net>
2016-04-01 13:46:04 -0400

    Fix relative import path issue in Typeahead

--- src/ui/typeahead.js
+++ src/ui/typeahead.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import _ from 'lodash'
-import API from 'api'
+import API from '../api'
 
 export default class Typeahead extends React.Component {
 
