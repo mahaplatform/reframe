7fac27b3aa6ae0cdef4883914e4631e0fce54456
Armand Zerilli <armand@zeril.li>
2016-04-04 18:03:07 -0400

    Make Typeaheads polymorphic on presence of "selectable" prop

--- src/ui/typeahead/index.js
+++ src/ui/typeahead/index.js
@@ -1,3 +1,18 @@
+import React from 'react'
 import Typeahead from './core'
+import SelectableTypeahead from './selectable'
 
-export default Typeahead
+export default class PolymorphicTypeahead extends React.Component {
+  render() {
+    if(this.props.selectable) {
+      return <SelectableTypeahead ref="t" {...this.props}/>
+    }
+    else {
+      return <Typeahead ref="t" {...this.props}/>
+    }
+  }
+
+  clear() {
+    this.refs.t.clear()
+  }
+}
