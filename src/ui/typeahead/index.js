af93f1bd2b058afc25a1de4cf58a79b18b7a23b5
Armand Zerilli <armand@zerilliworks.net>
2016-04-07 17:17:13 -0400

    Implement the new CategoryTypeahead

--- src/ui/typeahead/index.js
+++ src/ui/typeahead/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Typeahead from './core'
 import SelectableTypeahead from './selectable'
+import CategoryTypeahead from './category'
 
 export default class PolymorphicTypeahead extends React.Component {
   render() {
     if(this.props.selectable) {
-      return <SelectableTypeahead ref="t" {...this.props}/>
+      return <SelectableTypeahead ref="t" {...this.props} />
+    }
+    else if(this.props.categories) {
+      return <CategoryTypeahead ref="t" {...this.props} />
     }
     else {
-      return <Typeahead ref="t" {...this.props}/>
+      return <Typeahead ref="t" {...this.props} />
     }
   }
 
