f4d7e0d0917f7af388c75b4b4816591c4f7410b5
Armand Zerilli <armand@zeril.li>
2016-04-20 11:28:34 -0400

    Add loading/merging methods to Config utility

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,6 +5,10 @@ let globalConfig = _.assign({}, _.cloneDeep(defaultConfig))
 
 export default class Config {
   static load(cfg) {
+    globalConfig = _.merge(globalConfig, _.cloneDeep(cfg))
+  }
+
+  static overwrite(cfg) {
     globalConfig = _.cloneDeep(cfg)
   }
 
