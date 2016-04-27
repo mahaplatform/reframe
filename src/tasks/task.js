41c871e701a621b1e692f9a96196f7e96f3b2fd4
mochini <greg@thinktopography.com>
2016-04-27 11:51:07 -0400

    fix target

--- src/tasks/task.js
+++ src/tasks/task.js
@@ -8,7 +8,7 @@ class Task extends React.Component {
     if(this.props.task.handler) {
       return <a onClick={this.props.task.handler} activeClassName="active" className="item">{this.props.task.label}</a>
     } else if(this.props.task.url) {
-      let target = (this.props.task.target && this.props.task.url == 'blank') ? '_blank' : '_self'
+      let target = (this.props.task.target && this.props.task.target == 'blank') ? '_blank' : '_self'
       return <a href={this.props.task.url} target={target} activeClassName="active" className="item">{this.props.task.label}</a>
     } else if(this.props.task.route) {
       return <IndexLink to={this.props.task.route} activeClassName="active" className="item">{this.props.task.label}</IndexLink>
