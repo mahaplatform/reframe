8b7c231489829aed7db0d8da76c747ee3c24b91d
Greg Kops <greg@thinktopography.com>
2017-12-06 15:42:12 -0500

    work on wysiwyg

--- src/controls/wysiwyg/index.js
+++ src/controls/wysiwyg/index.js
@@ -1,4 +1,5 @@
 import { CompositeDecorator, Editor, EditorState, RichUtils } from 'draft-js'
+import { CSSTransition } from 'react-transition-group'
 import { convertFromHTML, convertToHTML } from 'draft-convert'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -23,60 +24,72 @@ class Wysiwyg extends React.Component {
     ])
     this.state = {
       editorState: EditorState.createEmpty(decorator),
-      showURLInput: false,
+      link: false,
       urlValue: ''
     }
   }
 
   render() {
+    const { editorState, link } = this.state
     return (
-      <div className="maha-wysiwyg">
-        <div className="maha-wysiwyg-header">
-          <div className="maha-wysiwyg-header-section">
-            <div className="maha-wysiwyg-header-icon" onClick={ this._handleBlockType.bind(this, 'header-one') }>
+      <div className="reframe-wysiwyg">
+        <CSSTransition key="reframe-wysiwyg-modal-overlay" in={ link } classNames="expanded" timeout={ 500 } mountOnEnter={ true } unmountOnExit={ true }>
+          <div className="reframe-wysiwyg-modal-overlay" onClick={ this._handleLinkClose.bind(this )} />
+        </CSSTransition>
+        <CSSTransition key="reframe-wysiwyg-modal" in={ link } classNames="expanded" timeout={ 500 } mountOnEnter={ true } unmountOnExit={ true }>
+          <div className="reframe-wysiwyg-modal">
+            <input type="text" placeholder="Type a link..." ref={ node => this.link = node } onKeyDown={ this._handleLinkKeyDown.bind(this) } />
+          </div>
+        </CSSTransition>
+        <div className="reframe-wysiwyg-header">
+          <div className="reframe-wysiwyg-header-section">
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleBlockType.bind(this, 'header-one') }>
               <i className="fa fa-fw fa-header" />
             </div>
-            <div className="maha-wysiwyg-header-icon" onClick={ this._handleInlineStyle.bind(this, 'BOLD') }>
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleInlineStyle.bind(this, 'BOLD') }>
               <i className="fa fa-fw fa-bold" />
             </div>
-            <div className="maha-wysiwyg-header-icon" onClick={ this._handleInlineStyle.bind(this, 'ITALIC') }>
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleInlineStyle.bind(this, 'STRIKETHROUGH') }>
+              <i className="fa fa-fw fa-strikethrough" />
+            </div>
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleInlineStyle.bind(this, 'ITALIC') }>
               <i className="fa fa-fw fa-italic" />
             </div>
-            <div className="maha-wysiwyg-header-icon" onClick={ this._handleInlineStyle.bind(this, 'UNDERLINE') }>
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleInlineStyle.bind(this, 'UNDERLINE') }>
               <i className="fa fa-fw fa-underline" />
             </div>
           </div>
-          <div className="maha-wysiwyg-header-section">
-            <div className="maha-wysiwyg-header-icon" onClick={ this._handleLink.bind(this) }>
+          <div className="reframe-wysiwyg-header-section">
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleLinkBegin.bind(this) }>
               <i className="fa fa-fw fa-link" />
             </div>
-            <div className="maha-wysiwyg-header-icon" onClick={ this._handleUnlink.bind(this) }>
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleUnlink.bind(this) }>
               <i className="fa fa-fw fa-unlink" />
             </div>
           </div>
-          <div className="maha-wysiwyg-header-section">
-            <div className="maha-wysiwyg-header-icon" onClick={ this._handleBlockType.bind(this, 'ordered-list-item') }>
+          <div className="reframe-wysiwyg-header-section">
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleBlockType.bind(this, 'ordered-list-item') }>
               <i className="fa fa-fw fa-list-ol" />
             </div>
-            <div className="maha-wysiwyg-header-icon" onClick={ this._handleBlockType.bind(this, 'unordered-list-item') }>
+            <div className="reframe-wysiwyg-header-icon" onClick={ this._handleBlockType.bind(this, 'unordered-list-item') }>
               <i className="fa fa-fw fa-list-ul" />
             </div>
           </div>
-          <div className="maha-wysiwyg-header-section">
-            <div className="maha-wysiwyg-header-icon">
+          <div className="reframe-wysiwyg-header-section">
+            <div className="reframe-wysiwyg-header-icon">
               <i className="fa fa-fw fa-align-left" />
             </div>
-            <div className="maha-wysiwyg-header-icon">
+            <div className="reframe-wysiwyg-header-icon">
               <i className="fa fa-fw fa-align-center" />
             </div>
-            <div className="maha-wysiwyg-header-icon">
+            <div className="reframe-wysiwyg-header-icon">
               <i className="fa fa-fw fa-align-right" />
             </div>
           </div>
         </div>
-        <div className="maha-wysiwyg-body">
-          <div className="maha-wysiwyg-page">
-            <Editor editorState={ this.state.editorState } onChange={ this._handleChange.bind(this) } />
+        <div className="reframe-wysiwyg-body">
+          <div className="reframe-wysiwyg-page">
+            <Editor ref={ node => this.el = node } editorState={ editorState } onChange={ this._handleChange.bind(this) } />
           </div>
         </div>
       </div>
@@ -115,26 +128,46 @@ class Wysiwyg extends React.Component {
     ))
   }
 
-  _handleLink() {
-    // const { editorState, urlValue } = this.state;
+  _handleLinkBegin() {
+    const link = true
+    this.setState({ link })
+    setTimeout(() => this.link.focus(), 500)
+  }
+
+  _handleLinkKeyDown(e) {
+    if(e.which === 13) this._handleLinkChange(e)
+  }
+
+  _handleLinkClose() {
+    this.setState({
+      link: false
+    }, () => {
+      setTimeout(() => this.el.focus(), 0)
+    })
+  }
+
+  _handleLinkChange(e) {
+    e.preventDefault()
     const { editorState } = this.state
-    const urlValue = 'http://google.com'
+    const url = e.target.value
     const contentState = editorState.getCurrentContent()
     const contentStateWithEntity = contentState.createEntity(
       'LINK',
       'MUTABLE',
-      { url: urlValue }
+      { url }
     )
     const entityKey = contentStateWithEntity.getLastCreatedEntityKey()
     const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity })
+    const replacement = RichUtils.toggleLink(
+      newEditorState,
+      newEditorState.getSelection(),
+      entityKey
+    )
     this.setState({
-      editorState: RichUtils.toggleLink(
-        newEditorState,
-        newEditorState.getSelection(),
-        entityKey
-      ),
-      showURLInput: false,
-      urlValue: ''
+      editorState: replacement,
+      link: false
+    }, () => {
+      setTimeout(() => this.el.focus(), 0)
     })
   }
 
