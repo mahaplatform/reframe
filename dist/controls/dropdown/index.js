6818c3c7e01538dfd1de5bfbd37830abb0b23ee8
Greg Kops <greg@thinktopography.com>
2018-06-13 13:47:14 -0400

    implementing remainder of dropdown

--- dist/controls/dropdown/index.js
+++ dist/controls/dropdown/index.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
@@ -41,8 +43,10 @@ var Dropdown = function (_React$Component) {
     }
 
     return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Dropdown.__proto__ || Object.getPrototypeOf(Dropdown)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
-      value: null
-    }, _temp), _possibleConstructorReturn(_this, _ret);
+      active: false,
+      value: null,
+      animating: false
+    }, _this._handleOpen = _this._handleOpen.bind(_this), _this._handleClose = _this._handleClose.bind(_this), _temp), _possibleConstructorReturn(_this, _ret);
   }
 
   _createClass(Dropdown, [{
@@ -50,35 +54,27 @@ var Dropdown = function (_React$Component) {
     value: function render() {
       var _this2 = this;
 
-      var value = this.state.value;
-      var _props = this.props,
-          options = _props.options,
-          placeholder = _props.placeholder;
+      var options = this.props.options;
 
-      var option = _lodash2.default.find(options, { 'value': value });
       return _react2.default.createElement(
         'div',
-        { className: 'reframe-dropdown ui compact menu' },
+        { className: 'reframe-dropdown' },
         _react2.default.createElement(
           'div',
-          { className: 'ui simple dropdown item' },
-          option ? _react2.default.createElement(
-            'span',
-            null,
-            option.text
-          ) : _react2.default.createElement(
-            'span',
-            null,
-            placeholder
+          { className: this._getDropdownClass(), onClick: this._handleOpen },
+          _react2.default.createElement(
+            'div',
+            { className: 'text', onClick: this._handleOpen },
+            this._getLabel()
           ),
           _react2.default.createElement('i', { className: 'dropdown icon' }),
           _react2.default.createElement(
             'div',
-            { className: 'menu' },
+            { className: this._getMenuClass() },
             options.map(function (option, index) {
               return _react2.default.createElement(
                 'div',
-                { className: 'item', key: 'option_' + index, onClick: _this2._handleClick.bind(_this2, option.value) },
+                _extends({ key: 'option_' + index }, _this2._getOption(option)),
                 option.text
               );
             })
@@ -89,32 +85,121 @@ var Dropdown = function (_React$Component) {
   }, {
     key: 'componentDidMount',
     value: function componentDidMount() {
-      var _props2 = this.props,
-          defaultValue = _props2.defaultValue,
-          onReady = _props2.onReady;
+      var _props = this.props,
+          defaultValue = _props.defaultValue,
+          onReady = _props.onReady;
 
       if (defaultValue) this.setValue(defaultValue);
+      document.addEventListener('mousedown', this._handleClose);
       onReady();
     }
   }, {
     key: 'componentDidUpdate',
     value: function componentDidUpdate(prevProps, prevState) {
-      var value = this.state.value;
+      var _this3 = this;
+
+      var _state = this.state,
+          active = _state.active,
+          value = _state.value;
 
       if (value !== prevState.value) {
         this.props.onChange(value);
       }
+      if (active !== prevState.active) {
+        this.setState({
+          animating: true
+        });
+        setTimeout(function () {
+          return _this3.setState({
+            animating: false
+          });
+        }, 250);
+      }
     }
   }, {
-    key: '_handleClick',
-    value: function _handleClick(value) {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      document.removeEventListener('mousedown', this._handleClose);
+    }
+  }, {
+    key: '_getDropdownClass',
+    value: function _getDropdownClass() {
+      var _state2 = this.state,
+          animating = _state2.animating,
+          active = _state2.active;
+
+      var classes = ['ui', 'selection', 'dropdown'];
+      if (active) classes.push('active');
+      if (active && !animating) classes.push('visible');
+      if (!active && animating) classes.push('visible');
+      return classes.join(' ');
+    }
+  }, {
+    key: '_getMenuClass',
+    value: function _getMenuClass() {
+      var _state3 = this.state,
+          active = _state3.active,
+          animating = _state3.animating;
+
+      var classes = ['menu', 'transition'];
+      if (!animating && !active) classes.push('hidden');
+      if (animating || active) classes.push('visible');
+      if (animating && active) classes.push('animating slide down in');
+      if (animating && !active) classes.push('animating slide down out');
+      return classes.join(' ');
+    }
+  }, {
+    key: '_getLabel',
+    value: function _getLabel() {
+      var value = this.state.value;
+      var _props2 = this.props,
+          options = _props2.options,
+          placeholder = _props2.placeholder;
+
+      var option = _lodash2.default.find(options, { value: value });
+      return option ? option.text : placeholder;
+    }
+  }, {
+    key: '_getOption',
+    value: function _getOption(option) {
+      return {
+        className: 'item',
+        onClick: this._handleChoose.bind(this, option.value)
+      };
+    }
+  }, {
+    key: '_handleOpen',
+    value: function _handleOpen(e) {
+      var active = this.state.active;
+
+      if (active || e.target.className === 'item') return;
+      this.setState({
+        active: true
+      });
+    }
+  }, {
+    key: '_handleClose',
+    value: function _handleClose(e) {
+      var active = this.state.active;
+
+      var reserved = ['item', 'text', 'dropdown icon', 'ui selection dropdown active visible'];
+      if (!active || _lodash2.default.includes(reserved, e.target.className)) return;
+      this.setState({
+        active: false
+      });
+    }
+  }, {
+    key: '_handleChoose',
+    value: function _handleChoose(value) {
       this.setValue(value);
-      //.reframe-dropdown .dropdown .menu
     }
   }, {
     key: 'setValue',
     value: function setValue(value) {
-      this.setState({ value: value });
+      this.setState({
+        value: value,
+        active: false
+      });
     }
   }]);
 
@@ -125,12 +210,15 @@ Dropdown.propTypes = {
   defaultValue: _propTypes2.default.string,
   options: _propTypes2.default.array,
   placeholder: _propTypes2.default.string,
-  onChange: _propTypes2.default.func
+  onBusy: _propTypes2.default.func,
+  onChange: _propTypes2.default.func,
+  onReady: _propTypes2.default.func
 };
 Dropdown.defaultProps = {
   placeholder: 'Select an option...',
   defaultValue: null,
   options: [],
+  onBusy: function onBusy(value) {},
   onChange: function onChange(value) {},
   onReady: function onReady() {}
 };
--- src/controls/dropdown/index.js
+++ src/controls/dropdown/index.js
@@ -8,36 +8,41 @@ class Dropdown extends React.Component {
     defaultValue: PropTypes.string,
     options: PropTypes.array,
     placeholder: PropTypes.string,
-    onChange: PropTypes.func
+    onBusy: PropTypes.func,
+    onChange: PropTypes.func,
+    onReady: PropTypes.func
   }
 
   static defaultProps = {
     placeholder: 'Select an option...',
     defaultValue: null,
     options: [],
+    onBusy: (value) => {},
     onChange: (value) => {},
     onReady: () => {}
   }
 
   state = {
-    value: null
+    active: false,
+    value: null,
+    animating: false
   }
 
+  _handleOpen = this._handleOpen.bind(this)
+  _handleClose = this._handleClose.bind(this)
+
   render() {
-    const { value } = this.state
-    const { options, placeholder } = this.props
-    const option = _.find(options, { 'value': value })
+    const { options } = this.props
     return (
-      <div className="reframe-dropdown ui compact menu">
-        <div className="ui simple dropdown item">
-          { option ?
-            <span>{ option.text }</span> :
-            <span>{ placeholder }</span>
-          }
+      <div className="reframe-dropdown">
+        <div className={ this._getDropdownClass() } onClick={ this._handleOpen }>
+          <div className="text" onClick={ this._handleOpen }>{ this._getLabel() }</div>
           <i className="dropdown icon"></i>
-          <div className="menu">
+          <div className={ this._getMenuClass() }>
             { options.map((option, index) => (
-              <div className="item" key={`option_${index}`} onClick={ this._handleClick.bind(this, option.value) }>{ option.text }</div>
+              <div key={`option_${index}`} {...this._getOption(option)}>
+                { option.text }
+              </div>
             )) }
           </div>
         </div>
@@ -48,23 +53,88 @@ class Dropdown extends React.Component {
   componentDidMount() {
     const { defaultValue, onReady } = this.props
     if(defaultValue) this.setValue(defaultValue)
+    document.addEventListener('mousedown', this._handleClose)
     onReady()
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const { value } = this.state
-    if( value !== prevState.value ){
+    const { active, value } = this.state
+    if(value !== prevState.value) {
       this.props.onChange(value)
+    }
+    if(active !== prevState.active) {
+      this.setState({
+        animating: true
+      })
+      setTimeout(() => this.setState({
+        animating: false
+      }), 250)
     }
   }
 
-  _handleClick(value) {
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this._handleClose)
+  }
+
+  _getDropdownClass() {
+    const { animating, active } = this.state
+    const classes = ['ui','selection','dropdown']
+    if(active) classes.push('active')
+    if(active && !animating) classes.push('visible')
+    if(!active && animating) classes.push('visible')
+    return classes.join(' ')
+  }
+
+  _getMenuClass() {
+    const { active, animating } = this.state
+    const classes = ['menu','transition']
+    if(!animating && !active) classes.push('hidden')
+    if(animating || active) classes.push('visible')
+    if(animating && active) classes.push('animating slide down in')
+    if(animating && !active) classes.push('animating slide down out')
+    return classes.join(' ')
+  }
+
+  _getLabel() {
+    const { value } = this.state
+    const { options, placeholder } = this.props
+    const option = _.find(options, { value })
+    return option ? option.text : placeholder
+  }
+
+  _getOption(option) {
+    return {
+      className: 'item',
+      onClick: this._handleChoose.bind(this, option.value)
+    }
+  }
+
+  _handleOpen(e) {
+    const { active } = this.state
+    if(active || e.target.className === 'item') return
+    this.setState({
+      active: true
+    })
+  }
+
+  _handleClose(e) {
+    const { active } = this.state
+    const reserved = ['item','text','dropdown icon','ui selection dropdown active visible']
+    if(!active || _.includes(reserved, e.target.className)) return
+    this.setState({
+      active: false
+    })
+  }
+
+  _handleChoose(value) {
     this.setValue(value)
-    //.reframe-dropdown .dropdown .menu
   }
 
   setValue(value) {
-    this.setState({ value })
+    this.setState({
+      value,
+      active: false
+    })
   }
 
 }
